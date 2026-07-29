6f8ba4ebcdd52e43c63bf89b5b6108fc52ecdf4f
josue <santosolivjosue@gmail.com>
2026-07-29 15:01:09 -0300

    feat(modulo_02):atiadades

---
+++ modulo_01/atividade/ativiade-01-.py
@@ -0,0 +1,11 @@
+nome = 'josue '
+idade = 18
+curso = 'informatica'
+cidade = 'teresina' 
+ano_de_ingresso = 2026
+
+print ('nome:',nome)
+print ('idade: ',idade)
+print ('curso: ',curso)
+print ('cidade:',cidade)
+print ('ano de ingresso',ano_de_ingresso)
---
+++ modulo_01/atividade/atividade_02_.py
@@ -0,0 +1,7 @@
+# 1nome-invalido:começa com numero
+# nome aluno
+# nome-aluno
+# class
+# NomeAluno
+# nome_aluno
+# idade2
